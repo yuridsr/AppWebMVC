098c4c589ae9c1af7c5736e1ac0773b148a9b132
yuridsr <silvayuri529@gmail.com>
2024-03-19 19:53:48 -0300

    Adicionando controller, view e model para Departments

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+
+            list.Add(new Departament { Id = 1, name="Eletronics" });
+            list.Add(new Departament { Id = 2, name="Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+
+        public int Id { get; set; }
+        public string name { get; set; }
+
+
+    }
+}
